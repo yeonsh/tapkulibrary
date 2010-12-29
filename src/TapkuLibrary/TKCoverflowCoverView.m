dfa476544132166f0ab25dabbb295ba94bc6f730
yeonsh <yeonsh@gmail.com>
2010-12-29 23:09:20 +0900

    Changed to support transparent background.

--- src/TapkuLibrary/TKCoverflowCoverView.m
+++ src/TapkuLibrary/TKCoverflowCoverView.m
@@ -46,6 +46,96 @@
 @implementation TKCoverflowCoverView
 @synthesize image,baseline;
 
+#pragma mark - Image Reflection
+
+CGImageRef CreateGradientImage(int pixelsWide, int pixelsHigh)
+{
+	CGImageRef theCGImage = NULL;
+    
+	// gradient is always black-white and the mask must be in the gray colorspace
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+	
+	// create the bitmap context
+	CGContextRef gradientBitmapContext = CGBitmapContextCreate(NULL, pixelsWide, pixelsHigh,
+															   8, 0, colorSpace, kCGImageAlphaNone);
+	
+	// define the start and end grayscale values (with the alpha, even though
+	// our bitmap context doesn't support alpha the gradient requires it)
+	CGFloat colors[] = {0.0, 1.0, 1.0, 1.0};
+	
+	// create the CGGradient and then release the gray color space
+	CGGradientRef grayScaleGradient = CGGradientCreateWithColorComponents(colorSpace, colors, NULL, 2);
+	CGColorSpaceRelease(colorSpace);
+	
+	// create the start and end points for the gradient vector (straight down)
+	CGPoint gradientStartPoint = CGPointZero;
+	CGPoint gradientEndPoint = CGPointMake(0, pixelsHigh);
+	
+	// draw the gradient into the gray bitmap context
+	CGContextDrawLinearGradient(gradientBitmapContext, grayScaleGradient, gradientStartPoint,
+								gradientEndPoint, kCGGradientDrawsAfterEndLocation);
+	CGGradientRelease(grayScaleGradient);
+	
+	// convert the context into a CGImageRef and release the context
+	theCGImage = CGBitmapContextCreateImage(gradientBitmapContext);
+	CGContextRelease(gradientBitmapContext);
+	
+	// return the imageref containing the gradient
+    return theCGImage;
+}
+
+CGContextRef MyCreateBitmapContext(int pixelsWide, int pixelsHigh)
+{
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+	
+	// create the bitmap context
+	CGContextRef bitmapContext = CGBitmapContextCreate (NULL, pixelsWide, pixelsHigh, 8,
+														0, colorSpace,
+														// this will give us an optimal BGRA format for the device:
+														(kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst));
+	CGColorSpaceRelease(colorSpace);
+    
+    return bitmapContext;
+}
+
+- (UIImage *)reflectedImage:(UIImageView *)fromImage withHeight:(NSUInteger)height
+{
+    if(height == 0)
+		return nil;
+    
+	// create a bitmap graphics context the size of the image
+	CGContextRef mainViewContentContext = MyCreateBitmapContext(fromImage.bounds.size.width, height);
+	
+	// create a 2 bit CGImage containing a gradient that will be used for masking the 
+	// main view content to create the 'fade' of the reflection.  The CGImageCreateWithMask
+	// function will stretch the bitmap image as required, so we can create a 1 pixel wide gradient
+	CGImageRef gradientMaskImage = CreateGradientImage(1, height);
+	
+	// create an image by masking the bitmap of the mainView content with the gradient view
+	// then release the  pre-masked content bitmap and the gradient bitmap
+	CGContextClipToMask(mainViewContentContext, CGRectMake(0.0, 0.0, fromImage.bounds.size.width, height), gradientMaskImage);
+	CGImageRelease(gradientMaskImage);
+	
+	// In order to grab the part of the image that we want to render, we move the context origin to the
+	// height of the image that we want to capture, then we flip the context so that the image draws upside down.
+	CGContextTranslateCTM(mainViewContentContext, 0.0, height);
+	CGContextScaleCTM(mainViewContentContext, 1.0, -1.0);
+	
+	// draw the image into the bitmap context
+	CGContextDrawImage(mainViewContentContext, fromImage.bounds, fromImage.image.CGImage);
+	
+	// create CGImageRef of the main view bitmap content, and then release that bitmap context
+	CGImageRef reflectionImage = CGBitmapContextCreateImage(mainViewContentContext);
+	CGContextRelease(mainViewContentContext);
+	
+	// convert the finished reflection image to a UIImage 
+	UIImage *theImage = [UIImage imageWithCGImage:reflectionImage];
+	
+	// image is retained by the property setting above, so we can release the original
+	CGImageRelease(reflectionImage);
+	
+	return theImage;
+}
 
 - (id) initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
@@ -54,7 +144,7 @@
 		
 		[self addSubview:self.reflected];
 		[self addSubview:self.imageView];
-		[self addSubview:self.gradient];
+		//[self addSubview:self.gradient];
 		
 		self.layer.anchorPoint = CGPointMake(0.5, 0.5);
 		
@@ -88,8 +178,8 @@
 	
 	self.gradient.frame = CGRectMake(0, y + h, w, h);
 	
-	self.reflected.frame = CGRectMake(0, y + h, w, h);
-	self.reflected.image = image;
+	self.reflected.frame = CGRectMake(0, y + h, w, h*0.25);
+	self.reflected.image = [self reflectedImage:imageView withHeight:h*0.25];
 	//[self setNeedsDisplay];
 }
 - (void) setBaseline:(float)f{
@@ -108,7 +198,7 @@
 - (UIImageView*) reflected{
 	if(reflected==nil){
 		reflected =  [[UIImageView alloc] initWithFrame:CGRectMake(0, self.frame.size.width, self.frame.size.width, self.frame.size.width)];
-		reflected.transform = CGAffineTransformScale(reflected.transform, 1, -1);
+		//reflected.transform = CGAffineTransformScale(reflected.transform, 1, -1);
 	}
 	return reflected;
 }
@@ -124,6 +214,7 @@
 
 - (void) dealloc {
 	[image release];
+    [imageView release];
     [super dealloc];
 }
 
